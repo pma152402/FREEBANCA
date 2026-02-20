7497380dec6eca3796c9dead81d5ba8d7b904b81
pma152402 <pma152402@gmail.com>
2026-02-20 16:51:33 +0100

    feat: Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,17 +41,17 @@ export default function Navbar() {
 
     return (
         <>
-            <div ref={navRef} className={`bg-gradient-to-t from-cyan-700 to-cyan-900 fixed top-0 left-0 w-full z-200 text-gray-100 p-3 flex justify-between items-center transition-transform duration-500 border-b-2 border-cyan-200/40 shadow-lg shadow-cyan-500/60
+            <div ref={navRef} className={`bg-gradient-to-t from-cyan-700 to-cyan-900 fixed top-0 left-0 w-full z-200 text-gray-100 px-5 py-1 flex justify-between items-center transition-transform duration-500 border-b-2 border-cyan-200/40 shadow-lg shadow-cyan-500/60
                 ${visibleNav ? "translate-y-0" : "-translate-y-full"}`}>
 
-                <div className="flex flex-col items-center pl-5">
-                    <h6 className="text-2xl font-semibold border-b-2 border-gray-200">
+                <div className="flex flex-col items-center">
+                    <h6 className="sm:text-xl md:text-2xl font-semibold border-b-2 border-gray-200">
                         FREEBANCA
                     </h6>
                 </div>
 
 
-                <ul className="flex gap-20 items-center">
+                <ul className="flex gap-4 items-center md:text-lg sm:text-sm">
                     <li className="enlaces-navbar"> <IconGraph size={22}/> Productos</li>
                     <li className="enlaces-navbar"> <IconBuildingStore size={22}/>Mercado</li>
                     <li className="enlaces-navbar"> <IconMailOpenedFilled size={22}/>Contacto</li>
